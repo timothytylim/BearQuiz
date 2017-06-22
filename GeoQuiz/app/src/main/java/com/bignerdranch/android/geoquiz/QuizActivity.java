e4c3648da1494d24875f79d0e5d24d267e0809b0
Tim <timothylim@berkeley.edu>
2017-06-22 16:01:59 -0700

    Initial Commit

---
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast trueButton = Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT);
+                trueButton.setGravity(Gravity.TOP, 0, 0);
+                trueButton.show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast falseButton = Toast.makeText(QuizActivity.this, R.string.correct_toast,
+                        Toast.LENGTH_SHORT);
+                falseButton.setGravity(Gravity.TOP, 0, 0);
+                falseButton.show();
+            }
+        });
+
+    }
+}
